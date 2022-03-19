3a06df4eefd9bc09fb58e5194044e5c41fc82554
pratikghagare175 <pratikghagare1797@gmail.com>
2022-03-19 11:17:11 +0530

    added multipointer with sum zero problem

---
+++ Udemy/multiple-pointers/sumZero.js
@@ -0,0 +1,24 @@
+// SUM ZERO - RETURN THE FIRST PAIR OF NUMBERS WHOSE SUM IS ZERO
+
+const sumZero = (arr) => {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    const sum = arr[left] + arr[right];
+
+    //? if sum is zero return that pair and exit the loop
+    if (sum === 0) return [arr[left], arr[right]];
+
+    if (sum > 0) {
+      //? if the sum is greater than zero i.e on the positive side, then decrease the right pointer by 1
+      right--;
+    } else {
+      //? if the sum is less than zero i.e on the negative side, then increase the left pointer by 1
+      left++;
+    }
+  }
+};
+
+const check = [-6, -3, -2, -1, 0, 2, 3, 4, 5];
+console.log(sumZero(check));
