e844b4bdad823acc31134549b2c0b32867bf1c53
pratikghagare175 <pratikghagare1797@gmail.com>
2022-06-13 22:02:14 +0530

    added binary search question in udemy

--- Grokking Algorithms/1.binary-search.js
+++ Grokking Algorithms/1.binary-search.js
@@ -1,10 +1,10 @@
+//* Time Complexity - log(N)
 const binary_search = (list, item) => {
   let low = 0;
   let high = list.length - 1;
   while (low <= high) {
-    let mid = low + high;
-    let guess = list[mid];
-
+    const mid = Math.floor((low + high) / 2);
+    const guess = list[mid];
     if (guess === item) return mid;
 
     if (guess > item) high = mid - 1;
